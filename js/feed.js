40e6dcc021cc0b668aa45394860447147505a75f
tijaraproai <tijaraproapp@gmail.com>
2026-03-07 10:33:40 +0100

    Update feed.js

--- js/feed.js
+++ js/feed.js
@@ -1,8 +1,8 @@
-const feed = document.getElementById("feed-container");
+const feedContainer = document.getElementById("feed-container");
 
-feed.innerHTML = `
+feedContainer.innerHTML = `
 <div class="video-item">
-<img src="https://images.unsplash.com/photo-1518444065439-e933c06ce9cd">
+<img src="https://images.unsplash.com/photo-1518444065439-e933c06ce9cd" style="width:100%;height:100%;object-fit:cover;">
 <div class="video-overlay">
 <h2>منتج ترند</h2>
 <button class="buy-button">اشتر الآن</button>

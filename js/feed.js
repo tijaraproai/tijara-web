633f255c367593d818e685858a6ddefda3f1e897
tijaraproai <tijaraproapp@gmail.com>
2026-03-07 01:13:02 +0100

    Update feed.js

--- js/feed.js
+++ js/feed.js
@@ -1,43 +1,7 @@
 const feedContainer = document.getElementById("feed-container");
 
-const videos = [
-{
-name:"سماعات بلوتوث",
-price:120,
-video:"https://www.w3schools.com/html/mov_bbb.mp4"
-},
-{
-name:"ساعة ذكية",
-price:250,
-video:"https://www.w3schools.com/html/movie.mp4"
-}
-];
-
-function renderFeed(){
-
-videos.forEach(v=>{
-
-const div = document.createElement("div");
-div.className = "video-item";
-
-div.innerHTML = `
-<video autoplay muted loop playsinline>
-<source src="${v.video}" type="video/mp4">
-</video>
-
-<div class="video-overlay">
-<h3>${v.name}</h3>
-<p>${v.price} د.ت</p>
-<button class="buy-button" onclick="addToCart('${v.name}',${v.price})">
-اشتري الآن
-</button>
+feedContainer.innerHTML = `
+<div style="height:100vh;background:black;color:white;display:flex;align-items:center;justify-content:center;font-size:30px;">
+FEED WORKING
 </div>
 `;
-
-feedContainer.appendChild(div);
-
-});
-
-}
-
-renderFeed();
